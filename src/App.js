03d527733ca0712f9633d0c2b61232cd6b35cd7a
tancred480 <tancredshaikh90@gmail.com>
2021-12-06 15:20:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useTranslation} from "react-i18next";
+import { useEffect, useState } from 'react';
+import {InputLabel,MenuItem,FormControl,Select} from "@material-ui/core"
 
 function App() {
+  const { t ,i18n} = useTranslation();  
+  const userName = "Tanveer Shaikh";
+  const [language,setLanguage] = useState('en');
+  const handleLangChange=(event)=>{
+    setLanguage(event.target.value);
+  }
+  const changeLang = (lang)=>{
+    i18n.changeLanguage(lang);
+  }
+  useEffect(() => {
+    changeLang(language);
+  }, [language])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <FormControl variant="standard" style={{flexDirection:"row"}}>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={language}
+          onChange={handleLangChange}
+          label="Age"
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value="en">
+            <em>English</em>
+          </MenuItem>
+          <MenuItem value="hi">Hindi</MenuItem>
+          <MenuItem value="ta">Tamil</MenuItem>
+          <MenuItem value="zh">Chinese</MenuItem>
+        </Select>
+      </FormControl>
+      <h2>{t("welcome_msg")}</h2>
+      <p>{t("say_hellow")} {t("my_name",{name:userName})}</p>
     </div>
   );
 }
